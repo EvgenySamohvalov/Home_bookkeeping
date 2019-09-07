56b9d9dbea45411b31029145ed5077f44b7d1f67
unknown <evgenysamohvalov1988@gmail.com>
2019-09-07 14:47:21 +0300

    feat: add two components (learn create component)

---
+++ src/app/my/my.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-my',//с помощью него можно запускать данный компонент и отображать в html
+    templateUrl: './my.component.html'//путь к этому компоненту
+})
+
+export class MyComponent {}
