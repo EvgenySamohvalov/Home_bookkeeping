3e42db327e8441aa09cd53756de1bc2d769cc87c
Евгений Самохвалов Николаевич <@LATSPB.ru>
2019-09-09 12:08:23 +0300

    feat: add two list(learn directive ngFor)

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -11,9 +11,15 @@ export class CarsComponent {
   }
   addCarStatus = false;
   carName = '';
+  cars = ['Fort','Audi','BMW'];
+  items = [{id:3, name: 'item 1'}, {id:6, name: 'item 2'}, {id:9, name: 'item 3'}]
   
   addCar() {
     this.addCarStatus = true;
+    this.cars.push(this.carName);
+    // добавляем в массив cars значение из инпута с carName;
+    this.carName = '';
+    //  Очищаем значение carName;
   }
 
 
