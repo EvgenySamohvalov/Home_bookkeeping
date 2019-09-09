413053b2e9e0e6207daff93c9a5c3af98e95c002
Евгений Самохвалов Николаевич <@LATSPB.ru>
2019-09-09 13:05:39 +0300

    feat: add list date (learn acquaintance payp)

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -14,18 +14,16 @@ export class CarsComponent {
   addCarStatus = false;
   carName = '';
   cars = ['Fort','Audi','BMW','Mersedes','Mazda', 'Lada'];
+  items = [
+    new Date(2015, 1, 2).toDateString(),
+    new Date(2011, 3, 4).toDateString(),
+    new Date(2017, 5, 3).toDateString(),
+    new Date(2000, 2, 5).toDateString(),
+  ]
   
   addCar() {
     this.addCarStatus = true;
     this.cars.push(this.carName);
-    // добавляем в массив cars значение из инпута с carName;
     this.carName = '';
-    //  Очищаем значение carName;
   }
-
-  setBigCarText (car: string) {
-    return car.length > 4 ? true : false;
-  }
-
-
 }
