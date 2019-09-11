050bebdbcf6ec2386754bf971142cdf7c096e9fa
Евгений Самохвалов Николаевич <@LATSPB.ru>
2019-09-11 13:42:09 +0300

    feat: learn ngSwitch derection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   headerText = 'Создание своей дерективы.';
+  items = [1, 2, 3, 4, 5, 6];
+  current = 1; // свойство по которому мы будем делать свич
+
+  onClick(number:number) {
+    this.current = number;
+  }
 }
