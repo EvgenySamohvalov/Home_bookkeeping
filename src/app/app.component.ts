b9fec65dec2331e5adaa1aa12e35c4cc33519a04
unknown <evgenysamohvalov1988@gmail.com>
2019-09-11 18:24:09 +0300

    feat: add list pipes(learn pipes)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,22 @@ import { Component } from '@angular/core';
     <h2>{{name}}</h2>
     <h2>{{name | lowercase}}</h2>
     <h2>{{name | uppercase}}</h2>
-    <h2>{{name | slice:0:3}}</h2>
+    <h2>{{name | slice:0:3 |  uppercase}}</h2>
+    <hr>
+    <h2>{{pi}}</h2>
+    <h2>{{pi | number}}</h2>
+    <h2>{{pi | number:'2.3-4'}}</h2> 
+    <hr>
+    <h2>{{money | currency:'USD'}}</h2>
+    <h2>{{money | currency:'EUR':true}}</h2>
+    <hr>
+    <h2>{{date | date:'fullDate'}}</h2>
+    <h2>{{date | date:'shortDate'}}</h2>
+    <h2>{{date | date:'shortTime'}}</h2>
+    <hr>
+    <h2>{{amount | percent}}</h2>
+    <hr>
+    <pre><h2>{{object | json}}</h2></pre>
   </div>
   `
 })
@@ -15,7 +30,7 @@ export class AppComponent {
   name = 'WebForMySelf';
   pi = Math.PI;
   money = 350;
-  data = new Date();
+  date = new Date();
   amount = 0.45;
   object = {
     foo: 'bar',
