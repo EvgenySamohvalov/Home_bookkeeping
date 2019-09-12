4a1641f68abf727a32837a39f0eca8c878897172
Евгений Самохвалов Николаевич <@LATSPB.ru>
2019-09-12 12:49:46 +0300

    feat: add new service(learn use and create services)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,29 @@
 import { Component } from '@angular/core';
-// import 'rxjs/Rx';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <div class="col-xs-8 col-xs-offset-2">
-  <h1>{{asyncTitle | async}}</h1>
-    <input type="text" class="form-control" [(ngModel)]="searchCar">
-    <button class="btn-primery"(click)="addCar()">Добавить</button>
-    <hr>
-    <ul class="list-group">
-      <li 
-      class="list-group-item" 
-      *ngFor="let car of cars | carFilter:searchCar:'descr';let idx = index"
-      >
-        <b>{{idx + 1}}</b>
-        {{car.name}}
-        <i>{{car.descr}}</i>
-      </li>
-    </ul>
-  </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  searchCar: '';
   cars = [
-    {name: 'Ford', descr: 'WFM 1'},
-    {name: 'Mazda', descr: 'WFM 2'},
-    {name: 'Bently', descr: 'WFM 3'},
-    {name: 'Audi', descr: 'WFM 4'},
-    {name: 'Mersedes', descr: 'WFM 5'},
-    {name: 'BMW', descr: 'WFM 6'}
+    {
+      name: 'Ford',
+      isSold: false
+    },
+    {
+      name: 'Mazda',
+      isSold: true
+    },
+    {
+      name: 'Mercedes',
+      isSold: false
+    }
   ];
 
-  asyncTitle = Observable.of('Async title 3 seconds')
-  .delay(3000);
-
-  addCar() {
+  addCarToList(carName: string) {
     this.cars.push({
-      name: 'New car',
-      descr: 'WFM'
-    })
+      name: carName,
+      isSold: false
+    });
   }
 }
---
+++ src/app/console.service.ts
@@ -0,0 +1,5 @@
+export class ConsoleServise {
+    log(str: string) {
+        console.log(str);
+    }
+}
