9999425c329f3d059339690ca00f0d8f9e5962c6
Евгений Самохвалов Николаевич <@LATSPB.ru>
2019-09-16 10:50:03 +0300

    feat: cteate validation email and password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { promise } from 'protractor';
+import { reject } from 'q';
 
 
 @Component({
@@ -17,10 +19,15 @@ export class AppComponent implements OnInit{
 
   form: FormGroup;
 
+  charsCount = 5;
+
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      pass: new FormControl('', Validators.required),
+      user: new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email],this.checkForEmail),
+        pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
+  
+      }),
       country: new FormControl('ru'),
       answer: new FormControl('no')
     });
@@ -28,5 +35,28 @@ export class AppComponent implements OnInit{
 
   onSubmit() {
     console.log('Submit!',this.form);
+  }
+
+  checkForLength(control: FormControl) {
+    if(control.value.length <= this.charsCount) {
+      return {
+        'lengthError': true
+      };
+    }
+    return null;
+  }
+
+  checkForEmail(control: FormControl) : Promise<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'test@mail.ru') {
+          resolve({
+            'emailsUsed' : true
+          });
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
   }
 }
