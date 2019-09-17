eae5194bee2c6caa4e3d3bdd13f8590344f25d64
Евгений Самохвалов Николаевич <@LATSPB.ru>
2019-09-17 16:48:42 +0300

    feat: work with server (http GET and POST)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
 import { CarsService } from './cars.service';
 
+
+interface Cars {
+  name: string;
+  color: string;
+  id: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent  {
-  cars = [];
+  cars: Cars[] = [];
+  carName: string = '';
   constructor(private carsService: CarsService) {}
 
 
   loadCars() {
     this.carsService
     .getCars()
-    .subscribe((response)=> {
-      console.log(response);
+    .subscribe((cars: Cars[]) => {
+      this.cars = cars;
     });
+  }
+
+  addCar() {
+    this.carsService
+      .addCar(this.carName)
+      .subscribe((car: Cars) => {
+      this.cars.push(car);
+    });
+    this.carName = '';
   }
 }
--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Response } from "@angular/http";
+import { map } from "rxjs/operator/map";
 
 @Injectable() 
 export class CarsService {
     constructor(private http: Http) {}
 
     getCars() {
-        return this.http.get('http://localhost:3000/cars');
+        return this.http.get('http://localhost:3000/cars')
+        .map((response: Response) => response.json());
+    }
+
+    addCar(carName: string) {
+        const data = {
+            name:carName,
+            color:'pink'
+        };
+        return this.http.post('http://localhost:3000/cars', data)
+        .map((response: Response) => response.json());
     }
 }
