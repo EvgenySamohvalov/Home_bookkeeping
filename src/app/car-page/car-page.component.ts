a026fee431876a936ebdce1e9feab831fef64159
Евгений Самохвалов Николаевич <@LATSPB.ru>
2019-09-24 11:27:06 +0300

    feat: learn addres  string partr 2

--- src/app/car-page/car-page.component.ts
+++ src/app/car-page/car-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-car-page',
@@ -12,20 +12,40 @@ export class CarPageComponent implements OnInit {
   name: string;
   year: string;
   color: string;
+  hash: string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
     this.name = this.route.snapshot.params['name'];
     this.year = this.route.snapshot.queryParams['year'];
     this.color = this.route.snapshot.queryParams['color'];
+    this.hash = this.route.snapshot.fragment;
 
     this.route.params.subscribe((params: Params) => {
       this.id = params['id'];
       this.name = params['name'];
     });
-    // Это стрим, на который мы можем подписаться
+
+    this.route.queryParams.subscribe((params: Params) => {
+      this.color = params['color'];
+      this.year = params['year'];
+    })
+    // Для денамического обновления данных можем указать queryParams
+    // Это по сути стрим.
+
+    
+  }
+
+  openMazdaPage() {
+    this.router.navigate(['./cars', 8,'Mazda'], {
+      queryParams: {
+        color: 'pink',
+        year: '1995'
+      },
+      fragment: 'pic'
+    });
   }
 
 }
