1f12da07fb53cb82646193911e53fcc5d56e971e
Евгений Самохвалов Николаевич <@LATSPB.ru>
2019-09-25 13:42:55 +0300

    feat: learn lezy laud

--- src/app/cars-page/cars-routing.module.ts
+++ src/app/cars-page/cars-routing.module.ts
@@ -1,28 +1,20 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CarsPageComponent } from './cars-page.component';
 import { CarPageComponent } from './car-page/car-page.component';
 
-
 const carsRoutes: Routes = [
-    // тут мы опишем все роуты касающиеся машин
-    { path: 'cars', component: CarsPageComponent, children: [
-        { path: ':id/:name', component: CarPageComponent }
-      ] }
+  { path: '', component: CarsPageComponent, children: [
+    { path: ':id/:name', component: CarPageComponent }
+  ] }
 ];
 
-@NgModule ({
-    imports:[
-        RouterModule.forChild(carsRoutes)
-        // регестрируем модуль и отдаём его,
-        // но указываем что это не корень а ребёнок
-    ],
-    // то что мы отдаём
-    exports:[
-        RouterModule
-    ]
-    // то что мы принимаем
+@NgModule({
+  imports: [
+    RouterModule.forChild(carsRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-
 export class CarsRoutingModule {}
