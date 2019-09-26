2cd1df62ce1a5bbfb219f26c9f8f57360bc21324
Евгений Самохвалов Николаевич <@LATSPB.ru>
2019-09-26 13:52:29 +0300

    feat: learn ampty state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,11 @@
 import { Component } from '@angular/core';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from '@angular/animations';
+import { divTrigger } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [
-    trigger('clickedDiv', [
-      state('start', style({
-        backgroundColor: 'blue',
-        width: '150px',
-        height: '150px'
-      })),
-      state('end', style({
-        backgroundColor: 'red',
-        width: '300px',
-        height: '300px'
-      })),
-      state('active', style({
-        width: '170px',
-        height: '170px',
-        backgroundColor: 'orange'
-      })),
-      transition('start <=> end', animate('800ms ease-in')),
-      transition('start => active', animate(400)),
-      transition('active => end', animate(400))
-    ])
-  ]
+  animations: [divTrigger]
 })
 export class AppComponent {
-  clickedDivState = 'start';
-
-  changeDivState() {
-    this.clickedDivState = 'end';
-    setTimeout(() => {
-      this.clickedDivState = 'start';
-    }, 3000);
-  }
+  isVisible = false;
 }
